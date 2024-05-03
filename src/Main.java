517eaf4c454b0580b3007133afe1cc2cd7763707
Locku <lockup86@rambler.ru>
2024-05-03 21:07:06 +0500

    hm5.3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Задача №1
-        byte clientOS = 1;
+        byte clientOS =0;
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else if (clientOS == 1) {
@@ -12,70 +12,60 @@ public class Main {
         }
 
         // Задача №2
-        short clientDeviceYear = 2015;
-        if (clientDeviceYear >= 2015 && clientOS == 1) { // для того чтоб не путаться clientOS уже задан в Задаче №1
+        short clientDeviceYear = 2016;
+        if (clientDeviceYear < 2015 && clientOS == 1) { // для того чтоб не путаться clientOS уже задан в Задаче №1
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (clientDeviceYear < 2015 && clientOS == 1) {
+        } else if (clientDeviceYear >= 2015 && clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientDeviceYear >= 2015 && clientOS == 0) {
+        } else if (clientDeviceYear < 2015 && clientOS == 0) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else { // можно было бы как в 1ой задаче написать
             System.out.println("Установите версию приложения для Android по ссылке");
         }
         // Задача №3
-        short year = 1;
-        if (year % 100 == 0){
-            if (year % 400 == 0 ){
-                System.out.println(year + " год является високосным");
-            }
-        }else if (year % 4 == 0 && year >= 1584 ){
+        short year = 2000;
+        if (year % 4 == 0 && year >= 1584 && year % 100 != 0) {
+            System.out.println(year + " год является високосным");
+        }else if (year % 100 == 0 && year % 400 == 0) {
             System.out.println(year + " год является високосным");
         }else{
             System.out.println(year + " год не является високосным");
         }
 
-        // Задача №4
 
-        short deliveryDistance = 20 ;
-        String oneDay = "Потребуется одни сутки";
-        String twoDay = "Потребуется двое суток";
-        String threeDay = "Потребуется трое суток";
+        // Задача №4
+        short deliveryDistance = 100 ;
+        String oneDay = "потребуется одни сутки";
+        String twoDay = "потребуется двое суток";
+        String threeDay = "потребуется трое суток";
         String non = "Доставка отсутствует";
         boolean term = deliveryDistance <=20;
         boolean term2 = deliveryDistance > 20 && deliveryDistance <= 60;
         boolean term3 = deliveryDistance > 60 && deliveryDistance <=100;
         if (term){
-            System.out.println(oneDay);
+            System.out.println("Для доставки на растоянние: " + deliveryDistance + " км " + oneDay);
         } else if (term2) {
-            System.out.println(twoDay);
+            System.out.println("Для доставки на растоянние: " + deliveryDistance + " км " + twoDay);
         } else if (term3) {
-            System.out.println(threeDay);
+            System.out.println("Для доставки на растоянние: " + deliveryDistance + " км " + threeDay);
         }else{
             System.out.println(non);
         }
 
         // Задача №5
-        int monthNumber = 12;
+        int monthNumber = 9;
         switch (monthNumber) {
-            case 1:
-            case 2:
-            case 12:
-                System.out.println("Сезон зимы.");
+            case 1, 2, 12:
+                System.out.println(monthNumber + " месяц cезон зимы.");
                 break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("Сезон весны.");
+            case 3, 4, 5:
+                System.out.println(monthNumber + " месяц cезон весны.");
                 break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("Сезон лета.");
+            case 6, 7, 8:
+                System.out.println(monthNumber + " месяц cезон лета.");
                 break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("Сезон осени.");
+            case 9, 10, 11:
+                System.out.println(monthNumber + " месяц cезон осени.");
                 break;
             default:
                 System.out.println("");
